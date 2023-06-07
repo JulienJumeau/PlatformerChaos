d7d1c2c0c2dd336770fbde62f3cbcd0cdd1f22ea
Jumeau Julien <jumeau.ju@gmail.com>
2023-06-07 15:27:19 +0200

    Chaos : C++ DPlatform Player Collision + Timer for future Destruction

--- Source/PlatformerChaos/Private/DestructiblePlatform.cpp
+++ Source/PlatformerChaos/Private/DestructiblePlatform.cpp
@@ -2,4 +2,37 @@
 
 
 #include "DestructiblePlatform.h"
+#include "../PlatformerChaosCharacter.h" // Include the header file of ACharacter
+#include "GeometryCollection/GeometryCollectionComponent.h" 
 
+void ADestructiblePlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	GeometryCollectionComponent->OnComponentHit.AddDynamic(this, &ADestructiblePlatform::OnComponentHit);
+}
+
+void ADestructiblePlatform::OnComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (!OtherActor || !OtherActor->IsA(APlatformerChaosCharacter::StaticClass()))
+	{
+		return;
+	}
+
+	FVector characterLocation = OtherActor->GetActorLocation();
+	FVector characterUp = Cast<APlatformerChaosCharacter>(OtherActor)->GetActorUpVector();
+
+	FVector Direction = (Hit.ImpactPoint - characterLocation).GetSafeNormal();
+	float DotProduct = FVector::DotProduct(Direction, characterUp);
+
+	if (DotProduct < -0.5f)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Hit From above")));
+		GetWorldTimerManager().SetTimer(MyTimerHandle, this, &ADestructiblePlatform::ChaosDestroy, ChaosDestroyTimer, false);
+		GeometryCollectionComponent->OnComponentHit.RemoveDynamic(this, &ADestructiblePlatform::OnComponentHit);
+	}
+}
+
+void ADestructiblePlatform::ChaosDestroy()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Destroyed")));
+}
--- Source/PlatformerChaos/Public/DestructiblePlatform.h
+++ Source/PlatformerChaos/Public/DestructiblePlatform.h
@@ -7,11 +7,30 @@
 #include "DestructiblePlatform.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class PLATFORMERCHAOS_API ADestructiblePlatform : public AGeometryCollectionActor
 {
 	GENERATED_BODY()
-	
+
+public:
+	ADestructiblePlatform() = default;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UFUNCTION()
+		void OnComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	UFUNCTION()
+		void ChaosDestroy();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom | Chaos Destruction")
+		float ChaosDestroyTimer = 2.0f;
+
+	FTimerHandle MyTimerHandle;
 };
